f91e28e36bd8eb8afda956e2be0c6a61ba70b7fa
Brice Nkengsa <bricenkengsa@gmail.com>
2019-02-14 12:02:21 -0500

    simplify owner tests

--- src/OwnerTest.java
+++ src/OwnerTest.java
@@ -3,41 +3,29 @@ import org.junit.Test;
 
 public class OwnerTest {
   @Test
-  public void testGetName() {
-    Owner owner = new Owner("abc", "cde", 0.0);
-    assertEquals("abc", owner.getName());
-  }
-
-  @Test
-  public void testGetLocation() {
-    Owner owner = new Owner("abc", "cde", 0.0);
-    assertEquals("cde", owner.getLocation());
-  }
-
-  @Test
-  public void testGetRValue() {
-    Owner owner = new Owner("abc", "cde", 0.0);
-    assertEquals(0.0, owner.getRValue(), 0);
-  }
-  
-  @Test
   public void testSetName() {
     Owner owner = new Owner("abc", "cde", 0.0);
+
     owner.setName("xyz");
+
     assertEquals("xyz", owner.getName());
   }
 
   @Test
   public void testSetLocation() {
     Owner owner = new Owner("abc", "cde", 0.0);
+
     owner.setLocation("xyz");
+
     assertEquals("xyz", owner.getLocation());
   }
 
   @Test
   public void testSetRValue() {
     Owner owner = new Owner("abc", "cde", 0.0);
+
     owner.setRValue(1.0);
+
     assertEquals(1.0, owner.getRValue(), 0);
   }
 }
